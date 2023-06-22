b36a98c9d066cc881467b75285242869ac79a98f
cantoral18 <cantoralmaritza6@gmail.com>
2023-06-22 12:38:48 -0500

    clase22_06_23

--- CONTROLES DE FLUJO/main.py
+++ CONTROLES DE FLUJO/main.py
@@ -1,69 +1,91 @@
-##conversiones string numeros
-numero="10"
-numeroconvertido=int(numero)
-print(type(numero))
-print(type(numeroconvertido))
+# ##conversiones string numeros
+# numero="10"
+# numeroconvertido=int(numero)
+# print(type(numero))
+# print(type(numeroconvertido))
 
-flotanteString="10"
-flotanteNumero=float(flotantestring)
+# flotanteString="10"
+# flotanteNumero=float(flotantestring)
 
-numeroEntero=20
-numeroString=str(numeroEntero)
+# numeroEntero=20
+# numeroString=str(numeroEntero)
 
-print(type(numeroEntero))
-print(type(numeroString))
+# print(type(numeroEntero))
+# print(type(numeroString))
 
-##controles de fujo
-bloques
-cuando nosotros utilicemos cualquier control de flujo o funciones
-el codigo que se debe ejecutar despues debera estar definida por bloques o identificacines
-los programas se manejan de manera secuencial  
-
-
-##control de flujo :
-1.condicional .. se realiza algo si cumple cietas condiciones 
-cuando es verdad si se ejecuta,resuelve y cuando es falso 
-se salta al siguiente linea
-
-nombre"maritza"
-mensaje"hola"
-print mensaje
-print nombre
+# ##controles de fujo
+# bloques
+# cuando nosotros utilicemos cualquier control de flujo o funciones
+# el codigo que se debe ejecutar despues debera estar definida por bloques o identificacines
+# los programas se manejan de manera secuencial  
 
 
-#match apellido:
-#case apellido if apellido[2]=="e":
-#print(f"bienvenida")
-#case _:
-#print ("tu no eres de esta raza,eres")
+# ##control de flujo :
+# 1.condicional .. se realiza algo si cumple cietas condiciones 
+# cuando es verdad si se ejecuta,resuelve y cuando es falso 
+# se salta al siguiente linea
 
-##BLUCLES
-condicion=0
-wile condicion<11:
-    print(condicion)
-    condicion=condicion+1
+# nombre"maritza"
+# mensaje"hola"
+# print mensaje
+# print nombre
+
+
+# #match apellido:
+# #case apellido if apellido[2]=="e":
+# #print(f"bienvenida")
+# #case _:
+# #print ("tu no eres de esta raza,eres")
+
+# ##BLUCLES
+# condicion=0
+# wile condicion<11:
+#     print(condicion)
+#     condicion=condicion+1
     
-#ejercicio 1 pedir numeros pares:
-condicion=2
-while condicion<2
-    print(condicion)
-    condicion=condicion+2
+# #ejercicio 1 pedir numeros pares:
+# condicion=2
+# while condicion<2
+#     print(condicion)
+#     condicion=condicion+2
 
-#ejercicio2 
-edad=0
-while edad !=20:
-    edad=int(input("ingrese edad: "))
+# #ejercicio2 
+# edad=0
+# while edad !=20:
+#     edad=int(input("ingrese edad: "))
 
-#ejercicio3
-nombre=" "
-while nombre !=si:
-    nombre=input("ingese nombre: ")
-#otra forma de hacer:
-while True:
-nombre=input("como te llamas")
-if nombre =="si:"
-    break
+# #ejercicio3
+# nombre=" "
+# while nombre !=si:
+#     nombre=input("ingese nombre: ")
+# #otra forma de hacer:
+# while True:
+# nombre=input("como te llamas")
+# if nombre =="si:"
+#     break
+
+# #for numero in range(0,10):
+# # print (numero)
+
+vocales=["a","e","i","o","u"]
+for numero in range(0,5):
+    print(vocales[numero])
+
+for vocal in vocales:
+    print(vocal)
+
+## crear una lista de cinco colores
+
+colores=["azul","amarillo","rojo","negro","blanco"]
+    for numero in range(0,5):
+        print(numero[colores])
 
 
-#ejercicio  de juego
-condicion=
+colores=["azul","amarillo","rojo","negro","blanco"]
+
+for color in colores:
+    if color =="rojo":
+        print(encontrado)
+        break
+    print(color)
+        
